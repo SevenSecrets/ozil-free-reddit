8915d7e56be082b87c960bacdf520be2fcf9084e
Art <freddieshughes@googlemail.com>
2020-10-22 11:58:56 +0100

    getting down to the actual text of the title

--- contentScript.js
+++ contentScript.js
@@ -1,5 +1,11 @@
 var titles = document.getElementsByClassName("_eYtD2XCVieq6emjKBH3m");
 
 for (i = 0; i < titles.length; i++) {
-	console.log(titles[i])
+
+	var title = titles[i]
+	var titleText = title.innerHTML
+
+	if (titleText.includes("ozil") || titleText.inclues("özil")) {
+		// do nothing for now
+	}
 }
